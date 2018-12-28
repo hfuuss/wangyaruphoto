6c097dd85ecef67342f9692242276b423d7c24cc
y <y@front.dog>
2018-12-28 16:02:45 +0800

    初稿完成

--- src/components/GalleryPhoto.jsx
+++ src/components/GalleryPhoto.jsx
@@ -2,30 +2,100 @@ import React from 'react'
 import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
 
-import w1 from '../images/w1.png';
-import w2 from '../images/w2.png';
-import w3 from '../images/w3.png';
-import w4 from '../images/w4.png';
-
 
 const photos = [
   {
-    src: w1,
+    src: 'http://images.hfuusec.cn/18-12-28/36945969.jpg',
     width: 4,
     height: 5
   },
   {
-    src: w2,
+    src: 'http://images.hfuusec.cn/18-12-28/52902299.jpg',
     width: 1,
     height: 1
   },
   {
-    src: w3,
+    src: 'http://images.hfuusec.cn/18-12-28/61565077.jpg',
     width: 1,
     height: 1
   },
   {
-    src: w4,
+    src: 'http://images.hfuusec.cn/18-12-28/26270668.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/76561905.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/72791839.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/22930556.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/62974480.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/31005712.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/40711274.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/33408370.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/49708606.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/93613437.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/21467628.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/40159617.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/17058251.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/61565077.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/52902299.jpg',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'http://images.hfuusec.cn/18-12-28/37516178.jpg',
     width: 1,
     height: 1
   },
