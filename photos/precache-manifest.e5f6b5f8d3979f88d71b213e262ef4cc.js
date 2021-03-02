2578b489790e02892a069de242be51d8d813f93e
lippzhang <y@front.dog>
2021-03-02 15:18:17 +0800

    feat: 20210302重构

---
+++ photos/precache-manifest.e5f6b5f8d3979f88d71b213e262ef4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f6f2437c441c4b7336b",
+    "url": "/static/css/main.368b1dcb.chunk.css"
+  },
+  {
+    "revision": "2f6f2437c441c4b7336b",
+    "url": "/static/js/main.2f6f2437.chunk.js"
+  },
+  {
+    "revision": "86cb99861c813a7067f0",
+    "url": "/static/js/1.86cb9986.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "33a1ce3d3eb75a728eb25ecd6408cbe1",
+    "url": "/index.html"
+  }
+];
